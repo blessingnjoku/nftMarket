73f8b7d4d68d6d48b0317911b78415660c966c64
Blessing Njoku <blessingnjoku505@gmail.com>
2022-06-11 23:57:25 +0100

    sanity backend setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
